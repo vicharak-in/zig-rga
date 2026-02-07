35616b3d33216d4b0289a72b3de53796cf9f7bfa
vasu <vasucp1207@gmail.com>
2026-02-07 17:37:36 +0530

    api cleanpu

--- examples/crop.zig
+++ examples/crop.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
-const rga = @import("rga");
-const rga_utils = rga.RgaUtils;
-const im2d = rga.im2d;
+const rga = @import("rga").rga;
+const rga_utils = @import("rga").RgaUtils;
+const im2d = @import("rga").im2d;
 
 pub fn main() !void {
-    std.debug.print("RGA version: {d}\n", .{im2d.RGA_API_REVISION_VERSION});
+    std.debug.print("RGA version: {d}\n", .{im2d.im2d_version.RGA_API_REVISION_VERSION});
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
@@ -13,24 +13,24 @@ pub fn main() !void {
     var src_buf: []u8 = undefined;
     var dst_buf: []u8 = undefined;
 
-    var src_img: im2d.rga_buffer_t = undefined;
-    var dst_img: im2d.rga_buffer_t = undefined;
-    var src_handle: im2d.rga_buffer_handle_t = 0;
-    var dst_handle: im2d.rga_buffer_handle_t = 0;
+    var src_img: im2d.im2d_type.rga_buffer_t = undefined;
+    var dst_img: im2d.im2d_type.rga_buffer_t = undefined;
+    var src_handle: im2d.im2d_type.rga_buffer_handle_t = 0;
+    var dst_handle: im2d.im2d_type.rga_buffer_handle_t = 0;
 
     const src_width: usize = 1280;
     const src_height: usize = 720;
-    const src_format: usize = im2d.RK_FORMAT_RGB_888;
+    const src_format: usize = rga.RK_FORMAT_RGB_888;
 
     const dst_width: usize = 480;
     const dst_height: usize = 270;
-    const dst_format: usize = im2d.RK_FORMAT_RGB_888;
+    const dst_format: usize = rga.RK_FORMAT_RGB_888;
 
     const src_buf_size: usize = @intFromFloat(src_width * src_height * rga_utils.get_bpp_from_format(src_format));
     const dst_buf_size: usize = @intFromFloat(dst_width * dst_height * rga_utils.get_bpp_from_format(dst_format));
 
-    src_img = std.mem.zeroes(im2d.rga_buffer_t);
-    dst_img = std.mem.zeroes(im2d.rga_buffer_t);
+    src_img = std.mem.zeroes(im2d.im2d_type.rga_buffer_t);
+    dst_img = std.mem.zeroes(im2d.im2d_type.rga_buffer_t);
 
     src_buf = try alloc.alloc(u8, src_buf_size);
     defer alloc.free(src_buf);
@@ -42,29 +42,29 @@ pub fn main() !void {
     const file_size = try input_file.getEndPos();
     _ = try input_file.read(src_buf[0..file_size]);
 
-    var param: im2d.im_handle_param_t = undefined;
+    var param: im2d.im2d_type.im_handle_param_t = undefined;
     param.width = src_width;
     param.height = src_height;
-    param.format = im2d.RK_FORMAT_RGB_888;
+    param.format = rga.RK_FORMAT_RGB_888;
 
-    src_handle = im2d.importbuffer_virtualaddr(src_buf.ptr, &param);
-    dst_handle = im2d.importbuffer_virtualaddr(dst_buf.ptr, &param);
+    src_handle = im2d.im2d_buffer.importbuffer_virtualaddr(src_buf.ptr, &param);
+    dst_handle = im2d.im2d_buffer.importbuffer_virtualaddr(dst_buf.ptr, &param);
 
     if (src_handle == 0 or dst_handle == 0) {
         std.debug.print("importbuffer failed!\n", .{});
     }
 
-    src_img = im2d.wrapbuffer_handle(src_handle, src_width, src_height, src_format);
-    dst_img = im2d.wrapbuffer_handle(dst_handle, dst_width, dst_height, dst_format);
+    src_img = im2d.im2d_buffer.wrapbuffer_handle(src_handle, src_width, src_height, src_format);
+    dst_img = im2d.im2d_buffer.wrapbuffer_handle(dst_handle, dst_width, dst_height, dst_format);
 
-    var crop_rec: im2d.im_rect = undefined;
+    var crop_rec: im2d.im2d_type.im_rect = undefined;
     crop_rec.x = 256;
     crop_rec.y = 256;
     crop_rec.width = 480;
     crop_rec.height = 270;
-    ret = im2d.imcrop(src_img, dst_img, crop_rec);
+    ret = im2d.im2d_single.imcrop(src_img, dst_img, crop_rec);
 
-    if (ret != im2d.IM_STATUS_SUCCESS) {
+    if (ret != im2d.im2d_type.IM_STATUS_SUCCESS) {
         std.debug.print("imcrop failed\n", .{});
         return error.rgaError;
     }
--- examples/cvtcolor.zig
+++ examples/cvtcolor.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
-const rga = @import("rga");
-const rga_utils = rga.RgaUtils;
-const im2d = rga.im2d;
+const rga = @import("rga").rga;
+const rga_utils = @import("rga").RgaUtils;
+const im2d = @import("rga").im2d;
 
 pub fn main() !void {
-    std.debug.print("RGA version: {d}\n", .{im2d.RGA_API_REVISION_VERSION});
+    std.debug.print("RGA version: {d}\n", .{im2d.im2d_version.RGA_API_REVISION_VERSION});
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
@@ -13,24 +13,24 @@ pub fn main() !void {
     var src_buf: []u8 = undefined;
     var dst_buf: []u8 = undefined;
 
-    var src_img: im2d.rga_buffer_t = undefined;
-    var dst_img: im2d.rga_buffer_t = undefined;
-    var src_handle: im2d.rga_buffer_handle_t = 0;
-    var dst_handle: im2d.rga_buffer_handle_t = 0;
+    var src_img: im2d.im2d_type.rga_buffer_t = undefined;
+    var dst_img: im2d.im2d_type.rga_buffer_t = undefined;
+    var src_handle: im2d.im2d_type.rga_buffer_handle_t = 0;
+    var dst_handle: im2d.im2d_type.rga_buffer_handle_t = 0;
 
     const src_width: usize = 1280;
     const src_height: usize = 720;
-    const src_format: usize = im2d.RK_FORMAT_RGB_888;
+    const src_format: usize = rga.RK_FORMAT_RGB_888;
 
     const dst_width: usize = 1280;
     const dst_height: usize = 720;
-    const dst_format: usize = im2d.RK_FORMAT_BGR_888;
+    const dst_format: usize = rga.RK_FORMAT_BGR_888;
 
     const src_buf_size: usize = @intFromFloat(src_width * src_height * rga_utils.get_bpp_from_format(src_format));
     const dst_buf_size: usize = @intFromFloat(dst_width * dst_height * rga_utils.get_bpp_from_format(dst_format));
 
-    src_img = std.mem.zeroes(im2d.rga_buffer_t);
-    dst_img = std.mem.zeroes(im2d.rga_buffer_t);
+    src_img = std.mem.zeroes(im2d.im2d_type.rga_buffer_t);
+    dst_img = std.mem.zeroes(im2d.im2d_type.rga_buffer_t);
 
     src_buf = try alloc.alloc(u8, src_buf_size);
     defer alloc.free(src_buf);
@@ -42,31 +42,31 @@ pub fn main() !void {
     const file_size = try input_file.getEndPos();
     _ = try input_file.read(src_buf[0..file_size]);
 
-    var src_param = im2d.im_handle_param_t{
+    var src_param = im2d.im2d_type.im_handle_param_t{
         .width = src_width,
         .height = src_height,
         .format = src_format,
     };
 
-    var dst_param = im2d.im_handle_param_t{
+    var dst_param = im2d.im2d_type.im_handle_param_t{
         .width = dst_width,
         .height = dst_height,
         .format = dst_format,
     };
 
-    src_handle = im2d.importbuffer_virtualaddr(src_buf.ptr, &src_param);
-    dst_handle = im2d.importbuffer_virtualaddr(dst_buf.ptr, &dst_param);
+    src_handle = im2d.im2d_buffer.importbuffer_virtualaddr(src_buf.ptr, &src_param);
+    dst_handle = im2d.im2d_buffer.importbuffer_virtualaddr(dst_buf.ptr, &dst_param);
 
     if (src_handle == 0 or dst_handle == 0) {
         std.debug.print("importbuffer failed!\n", .{});
     }
 
-    src_img = im2d.wrapbuffer_handle(src_handle, src_width, src_height, src_format);
-    dst_img = im2d.wrapbuffer_handle(dst_handle, dst_width, dst_height, dst_format);
+    src_img = im2d.im2d_buffer.wrapbuffer_handle(src_handle, src_width, src_height, src_format);
+    dst_img = im2d.im2d_buffer.wrapbuffer_handle(dst_handle, dst_width, dst_height, dst_format);
 
-    ret = im2d.imcvtcolor(src_img, dst_img, src_format, dst_format, im2d.IM_COLOR_SPACE_DEFAULT);
+    ret = im2d.im2d_single.imcvtcolor(src_img, dst_img, src_format, dst_format, im2d.im2d_type.IM_COLOR_SPACE_DEFAULT);
 
-    if (ret != im2d.IM_STATUS_SUCCESS) {
+    if (ret != im2d.im2d_type.IM_STATUS_SUCCESS) {
         std.debug.print("imcvtcolor failed\n", .{});
         return error.rgaError;
     }
